85ac9bfdfa2bb93b5bcb65f6267cdabe6185cb02
zmarsan <133293017+zmarsan@users.noreply.github.com>
2024-04-20 22:04:02 -0400

    Update UserController.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -16,11 +16,25 @@ class UserController extends Controller
     }
 
     public function saveUser() {
+        $inputData = [
+            'firstName' => $_POST['fname'] ? $_POST['lname'] : false,
+            'lastName' => $_POST['fname'] ? $_POST['lname'] : false,
+        ];
+        $userData = $this->validateUser($inputData);
 
+        $user = new User();
+        $user->saveUser(
+            [
+                'fname' => $userData['fname'],
+                'lname' => $userData['lname'],
+            ]
+        );
     }
 
-    public function viewUsers() {
-        
+    public function usersView() {
+        include '/public/assets/views/main/about.php';
+        include '/public/assets/views/main/login.php';
+        exit();
     }
 
 }
